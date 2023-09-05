c0f58563b32997eadf4d68d291f9dd607dccc88c
donfortune <donfortunet.df@gmail.com>
2023-09-05 02:55:35 -0700

    new commits

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
@@ -18,6 +19,11 @@ char *create_array(unsigned int size, char c)
 
 	if (arr == NULL)
 		return (NULL);
+	
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
 
 
 	return (arr);
