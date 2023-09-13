f4382f5ae4ceb81e4e0a1f8ad94778d83ae2c0a1
donfortune <donfortunet.df@gmail.com>
2023-09-13 09:49:42 -0700

    new file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,20 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
+
 /**
- * print_name - prints a name.
- * @name: the name to be printed.
- * @f: function pointer.
- */
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: is pointer
+**/
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
 		f(name);
+	}
 }
