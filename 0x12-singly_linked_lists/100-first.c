42a4f5b36b96f3fe8d721277d4163d7eeb6b18ee
donfortune <donfortunet.df@gmail.com>
2023-09-21 09:38:48 -0700

    new commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * pre_main - function executed before main
+ * Return: no return.
+ */
+
+void pre_main() __attribute__ ((constructor))
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds node at the beginning of a list.
+ * @head: the first node.
+ * @str: arguments
+ * Return: return the value of.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
