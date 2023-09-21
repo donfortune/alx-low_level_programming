9c83b6a7ffbb57d8856d27faa2a72e19cc0c6e85
donfortune <donfortunet.df@gmail.com>
2023-09-21 09:47:53 -0700

    bug fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return.
  */
 
-void pre_main(void) __attribute__ ((constructor))
+void __attribute__ ((constructor)) pre_main()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
